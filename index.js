532b2b9b73aaad697d77fbde2b4e0701def316fc
0v1se <dev@0v1se.com>
2018-06-04 10:54:27 +0300

    added increaseTime

--- index.js
+++ index.js
@@ -48,3 +48,24 @@ module.exports.randomInt = randomInt;
 module.exports.randomAddress = function() {
   return randomHexString(40);
 }
+module.exports.increaseTime = function(duration) {
+  const id = Date.now()
+  return new Promise((resolve, reject) => {
+    web3.currentProvider.sendAsync({
+      jsonrpc: '2.0',
+      method: 'evm_increaseTime',
+      params: [duration],
+      id: id,
+    }, err1 => {
+      if (err1) return reject(err1)
+
+      web3.currentProvider.sendAsync({
+        jsonrpc: '2.0',
+        method: 'evm_mine',
+        id: id+1,
+      }, (err2, res) => {
+        return err2 ? reject(err2) : resolve(res)
+      })
+    })
+  })
+}
