f2f32029fa68b2ef087aa33905ee56e283741643
0v1se <dev@0v1se.com>
2019-02-13 12:55:24 +0300

    0.2.2 verifyBalanceChange

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-function bn(value) { return new web3.BigNumber(value) };
+const BN = web3.utils.BN;
+const decimals = new BN("1000000000000000000");
 
+function bn(value) { return new BN(value) };
+
+function assertEq(v1, v2) {
+  assert(new BN(v1).eq(new BN(v2)), "v1=" + v1.toString() + " v2=" + v2.toString());
+}
+
+module.exports.assertEq = assertEq;
 module.exports.verifyBalanceChange = async function(account, change, todo) {
-  let before = await web3.eth.getBalance(account);
+  let before = new BN(await web3.eth.getBalance(account));
   await todo();
-  let after = await web3.eth.getBalance(account);
-  let actual = before.sub(after)
-  assert(before.add(change).equals(after), "before: " + before.toFixed() + " after: " + after.toFixed() + " actual: " + actual.toFixed() + " test: " + bn(change).toFixed());
+  let after = new BN(await web3.eth.getBalance(account));
+  let actual = before.sub(after);
+  assertEq(change, actual);
 }
 
 module.exports.findLog = function(tx, name) {
